d71e174c9005aa947bc5ecb38aae8219b7d35491
Matt Ward <matthew.ward@byng-systems.com>
2014-08-30 20:01:14 +0100

    Missing commit for ArrayToObjectFactoryInterface.

---
+++ src/TheLgbtWhip/CollationApi/ArrayToObjectFactoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ArrayToObjectFactoryInterface.php
+ * Definition of interface ArrayToObjectFactoryInterface
+ * 
+ * Created 30-Aug-2014 19:39:50
+ *
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ * 
+ */
+namespace TheLgbtWhip\CollationApi;
+
+
+
+/**
+ * ArrayToObjectFactoryInterface
+ * 
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ */
+interface ArrayToObjectFactoryInterface
+{
+    
+    /**
+     * 
+     * @param array $data
+     * @return object
+     */
+    public function build(array $data);
+    
+}
