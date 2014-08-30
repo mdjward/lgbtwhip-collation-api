05fd45aac497bb3363f2dc10018ed2a7c74b58cf
Matt Ward <matthew.ward@byng-systems.com>
2014-08-30 19:35:04 +0100

    Optimised PostcodeResponseProcessor to take advantage of the shortcuts given in the MapIt JSON response.

--- src/TheLgbtWhip/CollationApi/Postcode/PostcodeResponseProcessor.php
+++ src/TheLgbtWhip/CollationApi/Postcode/PostcodeResponseProcessor.php
@@ -25,6 +25,16 @@ class PostcodeResponseProcessor implements ProcessorInterface
     /**
      * 
      */
+    const SHORTCUTS_KEY = "shortcuts";
+    
+    /**
+     * 
+     */
+    const PARLIAMENT_SHORTCUT_KEY = "WMC";
+    
+    /**
+     * 
+     */
     const AREAS_KEY = 'areas';
     
     /**
@@ -56,11 +66,38 @@ class PostcodeResponseProcessor implements ProcessorInterface
      */
     public function processRawData(array $rawData)
     {
+        if (!isset($rawData[self::AREAS_KEY])) {
+            return null;
+        }
+        
+        $areas = $rawData[self::AREAS_KEY];
+        
         if (
-            isset($rawData[self::AREAS_KEY])
-            && ($targetData = $this->findArea($rawData[self::AREAS_KEY])) !==  null
+            (
+                ($shortcut = $this->getShortcut($rawData)) !== null
+                && ($targetData = $this->getAreaByShortcut($shortcut, $areas)) !== null
+            )
+            || ($targetData = $this->findArea($rawData[self::AREAS_KEY])) !==  null
         ) {
             return (object) $targetData;
+        }
+        
+        return null;
+    }
+    
+    protected function getShortcut(array $rawData)
+    {
+        if (isset($rawData[self::SHORTCUTS_KEY][self::PARLIAMENT_SHORTCUT_KEY])) {
+            return $rawData[self::SHORTCUTS_KEY][self::PARLIAMENT_SHORTCUT_KEY];
+        }
+        
+        return null;
+    }
+    
+    protected function getAreaByShortcut($shortcut, array $areas)
+    {
+        if (isset($areas[($shortcut = (string) $shortcut)])) {
+            return $areas[$shortcut];
         }
         
         return null;
